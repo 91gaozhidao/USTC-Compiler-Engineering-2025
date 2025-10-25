283aa51c2e6ef900d7273bd475cc5bb50fce9231
91gaozhidao <1287171474@qq.com>
2025-10-25 20:44:33 +0800

    测试脚本更改

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf build
 mkdir build && cd build && cmake .. && make -j && sudo make install
-cd /home/ethangao/workspace/USTC-Compiler-Engineering/USTC-Compiler-Engineering-2025/tests/2-ir-gen/autogen
+cd /home/ethangao/workspace/USTC-Compiler-Engineering-2025/tests/2-ir-gen/autogen
 bash eval_lab2.sh
